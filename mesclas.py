4faab290a93cf91e9f605072501245016029e104
levymc <levymcruz@gmail.com>
2023-03-10 12:01:27 -0300

    nome do arquivo corrigido, tbm as conferencias

--- mesclas.py
+++ mesclas.py
@@ -2,7 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 from datetime import datetime
 import xlwings as xw
-import sqlite3, shutil, win32print, win32api, pend_new, os
+import win32com.client as win32
+import sqlite3, shutil, win32print, re, pend_new, os
 
 
 def tamanho(db):
@@ -75,22 +76,53 @@ class Mesclas(Toplevel):
                         try:
                             ocs = cursor.execute(f"SELECT * FROM ocs WHERE track_form173={idform173}").fetchall()
                             nome = cursor.execute(f"SELECT nome FROM operadores WHERE codigo={form_173_tudo[0][8]}").fetchall()[0]
-                            new = self.path_gerado + "3- Form_Controle Aplicação Tinta "+ form_173_tudo[0][4] + r".xlsx"
+                            contador = 1
                             mescla_n = tudo[i][1]
+                            
+                            print("AAAA", os.listdir(self.path_gerado))
+                            
+                            if os.listdir(self.path_gerado) == []:
+                                new = self.path_gerado + "3- Form_Controle Aplicação Tinta "+ form_173_tudo[0][4] +" - "+ str(contador) + r".xlsx"
+                            else:
+                                for nome_arquivo in os.listdir(self.path_gerado):
+                                    if re.search(form_173_tudo[0][4], nome_arquivo):
+                                        contador += 1
+                                        new = self.path_gerado + "3- Form_Controle Aplicação Tinta "+ form_173_tudo[0][4] +" - "+ str(contador) + r".xlsx"
+                                    else: new = self.path_gerado + "3- Form_Controle Aplicação Tinta "+ form_173_tudo[0][4] +" - "+ str(contador) + r".xlsx"
+                                    
+                            
                             print("mescla: ", mescla_n)
                             print('Tamanho: ', len(ocs))
                             if len(ocs) <= 15:
+                                self.area = 'A1:K56'
                                 if os.path.exists(self.path_gerado):
-                                    shutil.copyfile(self.path, new)
+                                    if os.path.isfile(str(new)):
+                                        contador += 1
+                                        shutil.copyfile(self.path, str(new))
+                                    else: shutil.copyfile(self.path, str(new))
                                 else: 
-                                    os.makedirs(self.path_gerado)
-                                    shutil.copyfile(self.path, new)
+                                    if os.path.isfile(str(new)):
+                                        contador += 1
+                                        os.makedirs(self.path_gerado)
+                                        shutil.copyfile(self.path, str(new))
+                                    else: 
+                                        os.makedirs(self.path_gerado)
+                                        shutil.copyfile(self.path, str(new))
                             else:
+                                self.area = 'A1:K103'
                                 if os.path.exists(self.path_gerado):
-                                    shutil.copyfile(self.path_maior, new)
+                                    if os.path.isfile(str(new)):
+                                        contador += 1
+                                        shutil.copyfile(self.path_maior, str(new))
+                                    else: shutil.copyfile(self.path_maior, str(new))
                                 else: 
-                                    os.makedirs(self.path_gerado)
-                                    shutil.copyfile(self.path_maior, new)
+                                    if os.path.isfile(str(new)):
+                                        contador += 1
+                                        os.makedirs(self.path_gerado)
+                                        shutil.copyfile(self.path_maior, str(new))
+                                    else:
+                                        os.makedirs(self.path_gerado)
+                                        shutil.copyfile(self.path_maior, str(new))
                         except:messagebox.showinfo(message='Provavelmente o código do operador está errado!')
 
                         excel_app = xw.App(visible=False)
@@ -98,6 +130,7 @@ class Mesclas(Toplevel):
                         wks = xw.sheets
                         ws = wks[0]
                         linha = 35
+                        
                         for i in ocs:
                             padrao = i[1][:9]
                             oc = padrao + i[1].replace(padrao, '/')
@@ -105,23 +138,31 @@ class Mesclas(Toplevel):
                             ws.range("F"+f"{linha}").value = oc
                             ws.range("I"+f"{linha}").value = i[2]
                             linha += 1
+                            
                         ws.range("I4").value = datetime.today().strftime('%m-%d-%Y')
                         ws.range("C3").value = str(mescla_n)
                         ws.range("C4").value = nome
                         ws.range("J3").value = form_173_tudo[0][4]
                         ws.range("K4").value = form_173_tudo[0][8]
+                        
+                        # Selecionar a área de impressão
+                        # print_area = ws.range(self.area)
+
+                        # # Definir a área de impressão
+                        # ws.api.PageSetup.PrintArea = print_area.address
+                        
                         wb.save()
                         wb.close()
                         excel_app.quit()
-                        # lista_impressoras = win32print.EnumPrinters(2) #printar isso pra descobrir a impressora!
-                        # impressora = lista_impressoras[3]
+                        lista_impressoras = win32print.EnumPrinters(2) #printar isso pra descobrir a impressora!
+                        impressora = lista_impressoras[3]
                         
                         # win32print.SetDefaultPrinter(impressora[2]) # Coloca em Default a impressora a ser utilizada
                         # win32api.ShellExecute(0, "print", agora+r".xlsx", None, self.path_gerado, 0)
                         # cursor.execute(f"UPDATE form_40 SET print={1} WHERE mescla='{mescla_n}'")
-                        banco.commit()
-                        cursor.close()
-                        banco.close()
+                        # banco.commit()
+                        # cursor.close()
+                        # banco.close()
                         print("IMPRIMIU!!!")
                         self.destroy()
                     else: 
