eba83bec0a344e231d1647360c38eceb4256a60d
levymc <levymcruz@gmail.com>
2023-03-13 13:28:59 -0300

    att os estilos do Form173 para ttkbootstrap

--- form_173.py
+++ form_173.py
@@ -1,13 +1,16 @@
 from tkinter import * 
-from tkinter import messagebox
+from tkinter import messagebox, ttk
 import hashlib, json, sqlite3, re
 from PIL import ImageTk, Image 
 from datetime import datetime
 import tkinter.font as font
+from ttkbootstrap import Style as BsStyle
+from ttkbootstrap.constants import *
+from ttkbootstrap.widgets import Frame
 
 
 
-class App(Toplevel):
+class App(Tk):
     def __init__(self, user, db):
         super().__init__()
         self.db = db
@@ -22,6 +25,29 @@ class App(Toplevel):
         self.image = Image.open(r"logo.png")
         self.img = ImageTk.PhotoImage(self.image)
         self.selected_option = IntVar()
+        
+        self.style = BsStyle(theme='flatly')
+        self.style.configure('FundoOC.TFrame', background='#041536')
+        self.style.configure('Principal.TFrame',
+                             background='#f0f5ff',
+                             )            
+        self.style.map('Custom.TButton', background=[('active', '#cbd8f2')], 
+          foreground=[('active', 'black')],
+          bordercolor=[('active', '#384a6e')]) ## O .map serve para configuração de estilos de estado (pressionado, ativo, ....)
+        self.style.configure('Custom.TButton', background='#294c91',  #.configure serve para configurações de estilo no geral
+                font=('Helvetica', 9, 'bold'),
+                foreground='white',
+                borderwidth=5,
+                relief='solid',
+                bordercolor='#cbd8f2')   
+        self.style.map('Limpar.TButton', background=[('active', '#b3c9f5')], 
+          foreground=[('active', 'white')])  
+        self.style.configure('Limpar.TButton',
+                            background='#cbd8f2',
+                            foreground='black',
+                            font=('Helvetica', 7, 'bold'),
+                             )
+        
         try:
             banco = sqlite3.connect(self.db)
             cursor = banco.cursor()
@@ -41,53 +67,55 @@ class App(Toplevel):
 
     def create_wigets(self):
         # Form 173 - Campo Principal
-        q1 = Frame(self, width = 700, height = 40, background="#f0f5ff")
+        q1 = ttk.Frame(self, width = 700, height = 460, style='Principal.TFrame')#, background="#f0f5ff"
         q1.place(x=0,y=0)
-        x = Label(q1, text="Form. 173 - Solicitação de Preparação de Tinta", font='Impact 16', foreground='black', background='#f0f5ff')
+        x = ttk.Label(q1, text="Form. 173 - Solicitação de Preparação de Tinta", font='Impact 16', foreground='black', background='#f0f5ff')
         x.place(x=20, y=10)
-        img_frame = Label(self,image=self.img, background='#f0f5ff')
+        img_frame = ttk.Label(q1,image=self.img, background='#f0f5ff')
         img_frame.place(x=0, y=399)
-        solicitante = Label(self, text="Solicitante\n(Requesting Person)", font='Helvetica 9 bold', foreground='#011336', background='#f0f5ff')
+        solicitante = ttk.Label(q1, text="Solicitante\n(Requesting Person)", font='Helvetica 9 bold', background='#f0f5ff')
         solicitante.place(x=40, y=60)
-        self.solicitante_field = Label(self, text=self.user+" - "+str(self.cod_operador), font='Helvetica 9 bold', foreground='#011336', background='#f0f5ff')
+        self.solicitante_field = ttk.Label(q1, text=self.user+" - "+str(self.cod_operador), font='Helvetica 9 bold', foreground='#011336', background='#f0f5ff')
         self.solicitante_field.place(x=190, y=60)
-        numero = Label(self, text="Formulário Nº\n(Form Nº)", font='Helvetica 9 bold', foreground='#011336', background='#f0f5ff')
+        numero = ttk.Label(q1, text="Formulário Nº\n(Form Nº)", font='Helvetica 9 bold', foreground='#011336', background='#f0f5ff')
         numero.place(x=40, y=120)
-        self.numero_field = Entry(self, highlightthickness=1)
+        self.numero_field = ttk.Entry(q1)
         self.numero_field.place(x=190, y=120, width=150, height=30)
-        pintor = Label(self, text="Código do Pintor\n(Painter)", font='Helvetica 10 bold', foreground='#011336', background='#f0f5ff')
+        pintor = ttk.Label(q1, text="Código do Pintor\n(Painter)", font='Helvetica 10 bold', foreground='#011336', background='#f0f5ff')
         pintor.place(x=40, y=180)
         self.hoje = datetime.today().strftime('%d-%m-%Y')
         self.agora = datetime.today().strftime('%d-%m-%Y %H:%M')
-        self.pintor_field = Entry(self, highlightthickness=1)
+        self.pintor_field = ttk.Entry(q1)
         validate_pintor = (self.pintor_field.register(self.validate_entry_text), '%P')
         self.pintor_field.config(validate='key', validatecommand=validate_pintor)
         self.pintor_field.place(x=190, y=180, width=150, height=30)
-        cemb = Label(self, text="CEMB Tinta\n(Paint CODE)", font='Helvetica 9 bold', foreground='#011336', background='#f0f5ff')
+        cemb = ttk.Label(q1, text="CEMB Tinta\n(Paint CODE)", font='Helvetica 9 bold', foreground='#011336', background='#f0f5ff')
         cemb.place(x=40, y=240)
         
-        self.cemb_field = Entry(self, highlightthickness=1)
+        self.cemb_field = ttk.Entry(q1) #, highlightthickness=1
         validate_cemb = (self.cemb_field.register(self.validate_entry_text), '%P')
         self.cemb_field.config(validate='key', validatecommand=validate_cemb)
         self.cemb_field.place(x=190, y=240, width=150, height=30)
         
-        qnt = Label(self, text="Quantidade Solicitada\n(Quantity Requested)", font='Helvetica 9 bold', foreground='#011336', background='#f0f5ff')
+        qnt = ttk.Label(q1, text="Quantidade Solicitada\n(Quantity Requested)", font='Helvetica 9 bold', foreground='#011336', background='#f0f5ff')
         qnt.place(x=40, y=300)
-        self.qnt_field = Entry(self, highlightthickness=1)
+        self.qnt_field = ttk.Entry(q1)
         validate_qnt = (self.qnt_field.register(self.validate_entry_text), '%P')
         self.qnt_field.config(validate='key', validatecommand=validate_qnt)
         self.qnt_field.place(x=190, y=300, width=150, height=30)
         
-        self.grama = Checkbutton(self, text="grama", variable=self.selected_option, onvalue=1, offvalue=0)
+        self.grama = Checkbutton(q1, text="grama", variable=self.selected_option, onvalue=1, offvalue=0)
+        self.grama.configure(foreground='#011336', background='#f0f5ff')
         self.grama.place(x=345, y=290)
-        self.mililitro = Checkbutton(self, text="mililitro", variable=self.selected_option, onvalue=2, offvalue=0)
+        self.mililitro = Checkbutton(q1, text="mililitro", variable=self.selected_option, onvalue=2, offvalue=0)
+        self.mililitro.configure(foreground='#011336', background='#f0f5ff')
         self.mililitro.place(x=345, y=320)
         
-        botao = Button(self, text="Enviar Solicitação")
+        botao = ttk.Button(q1, text="Enviar Solicitação", style='Custom.TButton')
         botao.bind('<Button-1>', self.insert)
-        botao.place(x=330, y=370,height=30)
-        limpar = Button(self, text="Limpar Dados", command=self.limpar)
-        limpar.place(x=195, y=370,height=30)
+        botao.place(x=310, y=370,height=50)
+        limpar = ttk.Button(q1, text="Limpar Dados", command=self.limpar, style='Limpar.TButton')
+        limpar.place(x=35, y=360,height=30)
 
         self.numero_ocs = 0
         self.y = 50
@@ -129,25 +157,25 @@ class App(Toplevel):
             atualizar_contador()
 
         # Pendências - Campo direito, auxiliar
-        quadro = Frame(self, width = 250, height = 460, bg="#041536")
+        quadro = Frame(self, width = 250, height = 460, style='FundoOC.TFrame')#,bg="#041536"
         quadro.pack(side=RIGHT)
         
-        add_oc = Label(quadro, text=f"OC: ", foreground='white', background="#041536", font='Helvetica 9 bold')
+        add_oc = ttk.Label(quadro, text=f"OC: ", foreground='white', background="#041536", font='Helvetica 9 bold')
         add_oc.place(x=10, y=55)
-        qnt = Label(quadro, text="Qnt.: ", foreground='white', background="#041536", font='Helvetica 9 bold')
+        qnt = ttk.Label(quadro, text="Qnt.: ", foreground='white', background="#041536", font='Helvetica 9 bold')
         qnt.place(x=186, y=55)
-        self.oc_campo = Entry(quadro, highlightthickness=1, validate='key')
+        self.oc_campo = ttk.Entry(quadro, validate='key')
         validate_cmd = (self.oc_campo.register(self.validate_entry_text), '%P')
         self.oc_campo.config(validate='key', validatecommand=validate_cmd)
-        self.oc_campo.place(x=55, y=55)
-        self.qnt_campo = Entry(quadro, highlightthickness=1)
+        self.oc_campo.place(x=45, y=55, height=20)
+        self.qnt_campo = ttk.Entry(quadro)
         validate_qntOC = (self.qnt_campo.register(self.validate_entry_text), '%P')
         self.qnt_campo.config(validate='key', validatecommand=validate_qntOC)
-        self.qnt_campo.place(x=220, y=55, width=20)    
-        buttonAddOC = Button (quadro, font='Helvetica 8 bold', text="Adicionar OC", anchor='center', command=campo_oc,  bg='#99d199')
-        buttonAddOC.place(y=90, x=160, width=80, height=22)
+        self.qnt_campo.place(x=220, y=55, width=20, height=20)    
+        buttonAddOC = ttk.Button (quadro, text="Adicionar OC", command=campo_oc, style='Limpar.TButton')
+        buttonAddOC.place(y=90, x=160, width=84, height=25)
 
-        y = Label(quadro, text = "OC's utilizadas no lote: ",foreground='white', background="#041536", font='Impact 15')
+        y = ttk.Label(quadro, text = "OC's utilizadas no lote: ",foreground='white', background="#041536", font='Impact 15')
         y.place(x=30, y=10)
         
         def atualizar_contador():
@@ -155,7 +183,7 @@ class App(Toplevel):
 
         self.mylistbox=Listbox(quadro,width=35,height=6,  font='Trebuchet 9 bold', bg='white', selectmode=SINGLE)
         self.mylistbox.place(x=35,y=150, width=190, height=250)
-        infoOC = Label(quadro, text=f"{self.mylistbox.size()} OC's adicionadas", foreground='white', background="#041536", font='Helvetica 9 bold')
+        infoOC = ttk.Label(quadro, text=f"{self.mylistbox.size()} OC's adicionadas", foreground='white', background="#041536", font='Helvetica 9 bold')
         infoOC.place(x=33, y=405)
         deletarOC = Button(quadro, font=self.fonte_fa, text=u"\uf1f8", anchor='center', command=deletar_oc,  bg='red', fg='black')
         deletarOC.place(x=200, y=405)
@@ -236,7 +264,7 @@ class App(Toplevel):
         self.qnt_campo.delete(0, END)
         self.mylistbox.delete(0, END)
 
-# if __name__ == "__main__":
-#     app = App()
-#     app.mainloop()
+if __name__ == "__main__":
+    app = App('levymc', r"pintura.db")
+    app.mainloop()
 # App('teste')
