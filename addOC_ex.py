d297573db68e3fd2a9aca79385f18bd038c2a5c9
levymc <levymcruz@gmail.com>
2023-03-15 13:00:50 -0300

    botao "apagar OCs" adicionado

--- addOC_ex.py
+++ addOC_ex.py
@@ -61,8 +61,11 @@ class addOC_ex(Toplevel):
         self.linha_selecionada = {}
 
         # Cria o botão para carregar as informações da linha selecionada
-        btn_carregar = ttk.Button(self, text='Selecionar Formulário', command=lambda:self.carrega_linha_selecionada(), style='Att.TButton')
-        btn_carregar.pack(pady=20)
+        btn_carregar = ttk.Button(self, text='Adicionar OCs', command=lambda:self.carrega_linha_selecionada(), style='Att.TButton')
+        btn_carregar.pack(pady=20, padx=(180,0), side=LEFT)
+        
+        btn_deletar = ttk.Button(self, text='Apagar OCs', style='Att.TButton')
+        btn_deletar.pack(pady=20, padx=(0, 180), side=RIGHT)
         
     # Define uma função para o botão que carrega as informações da linha selecionada
     def carrega_linha_selecionada(self):
