310e18c2a6fd0ac856641b7ed9be0c7ff30d3842
levymc <levymcruz@gmail.com>
2023-04-20 16:40:37 -0300

    primeiras funcionalidades. userForm criado

--- web/static/js/scripts.js
+++ web/static/js/scripts.js
@@ -6,3 +6,11 @@ let renderizarLogin = () => {
     container.innerHTML = '';
     
 };
+
+let acessoUserForm = () => {
+    let userInput = document.querySelector(".userInput").value;
+    let passInput = document.querySelector(".passInput").value;
+    console.log(userInput);
+    console.log(passInput);
+    // aqui entra o axios para enviar o user e senha para conferir com o banco
+}
