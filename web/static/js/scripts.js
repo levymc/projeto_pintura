40a6d61795d956fdfe1cbc7aa9d3e474be940055
levymc <levymcruz@gmail.com>
2023-04-24 09:17:30 -0300

    criando a main com o menu superior

--- web/static/js/scripts.js
+++ web/static/js/scripts.js
@@ -7,7 +7,7 @@ let renderizarLogin = () => {
     container.innerHTML = `
     <div class="userForm flex">
         <input class="userInput" name="userInput" type="text" placeholder="Usuário">
-        <input class="passInput" name="passInput" type="text" placeholder="Senha">
+        <input class="passInput" name="passInput" type="password" placeholder="Senha">
         <button onclick="acessoUserForm()" id="btn-userForm">Entrar</button>
     </div>`
 };
@@ -20,13 +20,43 @@ let acessoUserForm = () => {
         senha: passInput,
     }).then(response => {
         console.log(response);
-        container.innerHTML = '';
+        renderizarMain();
     }).catch(error => {
         console.log(error);
         alert("Ocorreu algum erro, tente novamente ou acione o Processo.")
     })
 }
 
+let renderizarMain = () => {
+    container.innerHTML = '';
+    container.innerHTML += `
+            <div class="menu-superior">
+                <button onclick="renderizarForm173()">Solicitar Tinta</button>
+                <button onclick="renderizarForm40()">Preparação da Tinta</button>
+                <button onclick="renderizarForm161()">Aplicação da Tinta</button>
+            </div>
+            <div class="conteudo"></div>
+    `
+}
+
+let renderizarForm173 = () => {
+    let conteudo = document.querySelector(".conteudo");
+    conteudo.innerHTML = '';
+    conteudo.innerHTML += "Form 173";
+    
+}
+
+let renderizarForm40 = () => {
+    let conteudo = document.querySelector(".conteudo");
+    conteudo.innerHTML = '';
+    conteudo.innerHTML += "Form 40";
+}
+
+let renderizarForm161 = () => {
+    let conteudo = document.querySelector(".conteudo");
+    conteudo.innerHTML = '';
+    conteudo.innerHTML += "Form 161";
+}
 
 // Inicializando algumas funções
 renderizarLogin();
