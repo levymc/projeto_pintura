0e904e203f6ad551a777b4f732c6f2649bf42e24
levymc <levymcruz@gmail.com>
2023-05-16 11:55:02 -0300

    recebendo form173_tudo, ocs e nomePintor

--- print161.py
+++ print161.py
@@ -3,8 +3,9 @@ import xlwings as xw
 import win32com.client as win32
 import win32api
 import sqlite3, shutil, win32print, re, os, local
-from DBfuncs import DBForm_173, OCs
+from DBfuncs import DBForm_173, OCs, Operadores
 
 form_173_tudo = DBForm_173.consultaEspecifica(89, 'id')
 ocs = OCs.consultaEspecifica(89, 'track_form173')
-print(ocs)
+nomePintor = Operadores.consultaEspecificaCodigo(form_173_tudo[0]['codPintor'])[0]['nome']
+print(nomePintor)
