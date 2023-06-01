cb03abbe50140867abf04b938b0aba36a3cb8ae2
levymc <levymcruz@gmail.com>
2023-06-01 15:10:12 -0300

    criando a rota acesso

--- app.py
+++ app.py
@@ -26,41 +26,7 @@ app.register_blueprint(ultimaMescla_bp)
 def index():
     return render_template('index.html')
 
-@app.route("/acesso", methods=["POST", "GET"])
-def acesso():
-    userInput = request.json['usuario']
-    passInput = request.json['senha']
-    operador = Operadores.confereUsuario(userInput)
-    if operador is None:
-        print('Operador não encontrado.')
-        return abort(404)
-    elif operador.senha != hashlib.md5(passInput.encode()).hexdigest():
-        print('Senha incorreta.')
-        return abort(404)
-    else:
-        # renderiza a próxima tela
-        print('Acesso concedido.')
-        return "Ok"
-    
-@app.route("/acessoProcesso", methods=["POST", "GET"])
-def acessoProcesso():
-    userInput = request.json['userInput']
-    passInput = request.json['passInput']
-    operador = Operadores.confereUsuario(userInput)
-    if operador is None:
-        print('Operador não encontrado.')
-        return {"success": False}, abort(404)
-    elif operador.senha != hashlib.md5(passInput.encode()).hexdigest():
-        print('Senha incorreta.')
-        return {"success": False}, abort(404)
-    elif operador.priority != 'admin':
-        print('Acesso negado. Somente usuários com prioridade "admin" podem acessar.')
-        
-        return {"success": False}, abort(403)
-    else:
-        # Renderiza a próxima tela
-        print('Acesso concedido.')
-        return {"success": True}
+
 
 
 @app.route("/form173_inserir", methods=["POST", "GET"])
